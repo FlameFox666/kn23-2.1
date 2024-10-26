e4ddafa375e0460a2687aab97170ab51b7d2f7da
FlameFox666 <lexarole@gmail.com>
2024-10-26 12:57:05 +0300

    sass added

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,7 +1,23 @@
+let isOpen = false; 
+let asideOffset = "150px"
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  document.getElementById("mySidenav").style.width = asideOffset;
+  document.getElementById("overlay").style.display = "block";
+  document.body.style.overflow = 'hidden';
+}
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("overlay").style.display = "none";
+  document.body.style.overflow = 'auto';
+}
+
+function toggleNav() {
+  if (isOpen) {
+      closeNav();
+  } else {
+      openNav();
   }
+  isOpen = !isOpen;
+}
